d851e94c5e95c25e6576bed5e2f9a2d04d283a9f
SadeqShryim <smshryim60@gmail.com>
2025-04-13 04:53:33 -0400

    function 2 done
    completed function2 and the need part in main.

--- Stacks&Queues/Stacks&Queues/main.cpp
+++ Stacks&Queues/Stacks&Queues/main.cpp
@@ -7,8 +7,8 @@
 
 using namespace std;
 
-//Func.1 --> Brackets balanced
-bool is_balanced(string some_string);
+////Func.1 --> Brackets balanced
+//bool is_balanced(string some_string);
 
 //Func.2 --> Turn to binary
 string convert_to_binary(int some_int);
@@ -20,17 +20,43 @@ int timeDelay();
 
 
 int main() {
+	int num, cars, trafficTime;
+	cout << "What number would you lik eto convert to binary: ";
+	cin >> num;
 
+	string binary = convert_to_binary(num);
+	cout << binary << endl;
 
+	cout << "Simulation" << endl;
+	cout << "You are in traffic. How many cars are ahead: ";
+	cin >> cars;
+
+	trafficTime = getTimeToStart(cars);
+	cout << "You will start moving in " << trafficTime << " seconds." << endl;
 	return 0;
 }
-
-bool is_balanced(string some_string) {
-
-}
+//
+//bool is_balanced(string some_string) {
+//
+//}
 
 string convert_to_binary(int some_int) {
+	//base case
+	if (some_int == 0) return "0";
+	stack <int> into;
 
+	while (some_int > 0) {
+		into.push(some_int % 2);
+		some_int /= 2;
+	}
+
+	string outOf;
+	while (!into.empty()) {
+		outOf += to_string(into.top());
+		into.pop();
+	}
+
+	return outOf;
 }
 
 int getTimeToStart(int cars) {
