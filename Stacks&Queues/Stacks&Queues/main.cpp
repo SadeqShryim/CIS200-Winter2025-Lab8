6358aad472b1bc1dd032c0d8644410566cf4cc50
SadeqShryim <smshryim60@gmail.com>
2025-04-13 03:52:41 -0400

    Lab outline
    --included a helper function for time delay not included in directions

---
+++ Stacks&Queues/Stacks&Queues/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+//Func.1 --> Brackets balanced
+bool is_balanced(string some_string);
+
+//Func.2 --> Turn to binary
+string convert_to_binary(int some_int);
+
+//Func.3 --> Time simulator 
+	// Helper function for time delay
+int getTimeToStart(int cars);
+int timeDelay();
+
+
+int main() {
+
+
+	return 0;
+}
+
+bool is_balanced(string some_string) {
+
+}
+
+string convert_to_binary(int some_int) {
+
+}
+
+int getTimeToStart(int cars) {
+
+}
+
+int timeDelay() {
+
+}
